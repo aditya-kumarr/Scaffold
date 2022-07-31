da69104c6de2d50cdfbcf1864afa3ad519c4a4ae
aditya <adityakumar222333@gmail.com>
2022-07-31 17:02:12 +0530

    💥 NEW: pseudo coded the parts of a jsx file

---
+++ fileHandler.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+//add all the imports on top of the file
+const addImports = (params) => {};
+
+//create the main jsx function
+const createMain = (params) => {};
+
+//Add the important hooks here
+const addHooks = (params) => {};
+
+//Add important functions here
+const addFunctions = (params) => {};
+
+//Add the return JSX statment here
+const addReturn = (params) => {};
+
+//add the whole JSX here
+const addJSX = (params) => {};
+
+//add the export statement here
+const addExport = (params) => {};
+
+const template = () => {
+    
+}
