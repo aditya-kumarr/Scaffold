694f3a95a2b283c97e8c26ce36877146e6cd04a2
aditya <adityakumar222333@gmail.com>
2022-07-31 16:01:29 +0530

    ✨ New Feature: added the function to add page file

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const fs = require("node:fs");
-const scaffoldData = require('./scaffoldData.json');
-const CURRENT_DIR = process.cwd();
+const path = require("node:path");
+const scaffoldData = require("./scaffoldData.json");
+const CURRENT_PATH = process.cwd();
 const ARGS = process.argv.slice(2);
-
-
 
 const createFolders = (_folders) => {
   _folders.folders.forEach((folder) => {
-    if (!fs.existsSync(`${CURRENT_DIR}/${_folders.parent}/${folder}`)) {
-      fs.mkdir(`${CURRENT_DIR}/${_folders.parent}/${folder}`, (err) => {
+    const curPath = `${CURRENT_PATH}/${_folders.parent}/${folder}`;
+    console.log(curPath);
+    if (!fs.existsSync(curPath)) {
+      fs.mkdir(curPath, (err) => {
         if (err) {
-          console.warn("Not a react folder");
+          console.warn(err);
         }
       });
     }
+    createFiles(curPath);
   });
 };
-createFolders(scaffoldData.primaryFolders);
-createFolders(scaffoldData.mainComponents);
-createFolders(scaffoldData.mainPages);
+
+const createFiles = (folderPath) => {
+  const filePath = `${folderPath}/${path.basename(folderPath)}.jsx`
+  if(!fs.existsSync(filePath)) {
+    fs.writeFile(
+      filePath,
+      (err) => {
+        if (err) {
+          console.warn(filePath);
+        }
+      }
+    );
+  }
+};
+
+
+
+
+// createFolders(scaffoldData.folders.primaryFolders);
+// createFolders(scaffoldData.folders.mainComponents);
+createFolders(scaffoldData.folders.mainPages);
