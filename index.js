7a35851776e3f0363af5c82dfc98449ef4758acb
aditya <adityakumar222333@gmail.com>
2022-07-31 14:57:28 +0530

    🔨 Refactor Code: refactored the data to a json file

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const fs = require("node:fs");
-
+const scaffoldData = require('./scaffoldData.json');
 const CURRENT_DIR = process.cwd();
 const ARGS = process.argv.slice(2);
 
-// The main folders in the src folder
-const primaryFolders = {
-  parent: "src",
-  folders: [
-    "components",
-    "containers",
-    "hooks",
-    "pages",
-    "reducers",
-    "styles",
-    "utils",
-  ],
-};
-//the main components which are going to be used in almost every single project
-const mainComponents = {
-  parent: "src/components",
-  folders: ["navbar", "hero", "corousel", "footer", "forms", "data", "modal"],
-};
-//the main pages that are going to be used in almost every single project
-const mainPages = {
-  parent: "src/pages",
-  folders: ["home", "about", "contact", "login", "register"],
-};
+
 
 const createFolders = (_folders) => {
   _folders.folders.forEach((folder) => {
@@ -38,7 +16,6 @@ const createFolders = (_folders) => {
     }
   });
 };
-
-createFolders(primaryFolders);
-createFolders(mainComponents);
-createFolders(mainPages);
+createFolders(scaffoldData.primaryFolders);
+createFolders(scaffoldData.mainComponents);
+createFolders(scaffoldData.mainPages);
