4cc51544d8b842503bedb004d121ccffe887725f
aditya <adityakumar222333@gmail.com>
2022-07-31 14:14:34 +0530

    💥 NEW: added function to create folders from an array

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const ARGS = process.argv.slice(2);
 
 
 
+const createFolders = (folders = []) => {
+  folders.forEach((folder) => {
+    fs.mkdir(`${CURRENT_DIR}/src${folder}`, (err) => {
+      if (err) {
+        console.warn("Not a react folder");
+      }
+    });
+  });
+};
+
+createFolders(["/components", "/containers", "/pages", "/styles"]);
 
