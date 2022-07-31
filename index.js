87a828aea55480af67c85853194164dc647b6166
aditya <adityakumar222333@gmail.com>
2022-07-31 14:32:47 +0530

    💥 NEW: added most imprtant folders

--- index.js
+++ index.js
@@ -3,19 +3,42 @@ const fs = require("node:fs");
 const CURRENT_DIR = process.cwd();
 const ARGS = process.argv.slice(2);
 
+// The main folders in the src folder
+const primaryFolders = {
+  parent: "src",
+  folders: [
+    "components",
+    "containers",
+    "hooks",
+    "pages",
+    "reducers",
+    "styles",
+    "utils",
+  ],
+};
+//the main components which are going to be used in almost every single project
+const mainComponents = {
+  parent: "src/components",
+  folders: ["navbar", "hero", "corousel", "footer", "forms", "data", "modal"],
+};
+//the main pages that are going to be used in almost every single project
+const mainPages = {
+  parent: "src/pages",
+  folders: ["home", "about", "contact", "login", "register"],
+};
 
-
-
-
-const createFolders = (folders = []) => {
-  folders.forEach((folder) => {
-    fs.mkdir(`${CURRENT_DIR}/src${folder}`, (err) => {
-      if (err) {
-        console.warn("Not a react folder");
-      }
-    });
+const createFolders = (_folders) => {
+  _folders.folders.forEach((folder) => {
+    if (!fs.existsSync(`${CURRENT_DIR}/${_folders.parent}/${folder}`)) {
+      fs.mkdir(`${CURRENT_DIR}/${_folders.parent}/${folder}`, (err) => {
+        if (err) {
+          console.warn("Not a react folder");
+        }
+      });
+    }
   });
 };
 
-createFolders(["/components", "/containers", "/pages", "/styles"]);
-
+createFolders(primaryFolders);
+createFolders(mainComponents);
+createFolders(mainPages);
